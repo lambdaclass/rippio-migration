c162f72dcb56c372afdc753249da31ee9d91844a
Joaquin Carletti <joaquin.carletti@lambdaclass.com>
2023-12-21 11:54:54 -0300

    fix transaction

--- migration_test/test.go
+++ migration_test/test.go
@@ -17,23 +17,23 @@ var (
 	EthereumProvider = "https://rpc.ankr.com/eth_goerli" // goerli testnet
 )
 
-type MyTransaction struct {
-    *types.Transaction
-}
+// type MyTransaction struct {
+//     *types.Transaction
+// }
 
-func (tx *MyTransaction) WithSignature2() (types.Transaction) {
-	r := new(big.Int)
-	r.SetString("106287454196759066847211792031325706661401235312908304949829649037033979701705", 10)
-	s := new(big.Int)
-	s.SetString("7989732764615526430734405065785953595367951189355047462118874195239141735945", 10)
-	v := big.NewInt(int64(872))
-	chainId := big.NewInt(int64(418))
+// func (tx *MyTransaction) WithSignature2() (types.Transaction) {
+// 	r := new(big.Int)
+// 	r.SetString("106287454196759066847211792031325706661401235312908304949829649037033979701705", 10)
+// 	s := new(big.Int)
+// 	s.SetString("7989732764615526430734405065785953595367951189355047462118874195239141735945", 10)
+// 	v := big.NewInt(int64(872))
+// 	chainId := big.NewInt(int64(418))
 
-	cpy := tx.inner.Copy()
-	cpy.SetSignatureValues(chainId, v, r, s)
+// 	cpy := tx.inner.Copy()
+// 	cpy.SetSignatureValues(chainId, v, r, s)
 
-	return types.Transaction{inner: cpy, time: tx.time}
-}
+// 	return types.Transaction{inner: cpy, time: tx.time}
+// }
 
 func main() {
 	// Connect to zkSync network
@@ -56,29 +56,45 @@ func main() {
 	}
 	fmt.Println("Block number: ", blockNumber)
 
+	txInfo, _, err := client.TransactionByHash(context.Background(), common.HexToHash("0x4b59eb6bf0f63efdd62db864737fe00539f1aa278c9e51c9f3df7923869b0622"))
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println(txInfo)
+
 	// {"Nonce":0,"GasPrice":1000,"Gas":21000,"To":"0x08f5f9a336Aae6A72c795Ddf307864B13d13F0Aa","Value":1000000000000000000,"Input":null,"V":872,"R":106287454196759066847211792031325706661401235312908304949829649037033979701705,"S":7989732764615526430734405065785953595367951189355047462118874195239141735945,"Hash":"0x0785cb804d5e6db08d336922d68785f66de28d918da907ba1710b4c066eef477","From":"0x0000000000000000000000000000000000000000"}
 
 	address := new(common.Address)
 	*address = common.HexToAddress("0x08f5f9a336Aae6A72c795Ddf307864B13d13F0Aa")
 
+	r := new(big.Int)
+	r.SetString("106287454196759066847211792031325706661401235312908304949829649037033979701705", 10)
+	s := new(big.Int)
+	s.SetString("7989732764615526430734405065785953595367951189355047462118874195239141735945", 10)
 
-
-	transaction := MyTransaction{types.NewTx(
+	transaction := types.NewTx(
 		&types.DynamicFeeTx{
-			ChainID:   big.NewInt(int64(418)), // (*a.signer).Domain().ChainId,
-			Nonce:     0,                      // preparedTx.Nonce.Uint64(),
-			GasTipCap: big.NewInt(int64(0)),   // preparedTx.GasTipCap,
-			GasFeeCap: big.NewInt(int64(0)),   // preparedTx.GasFeeCap,
-			Gas:       21000,                  // preparedTx.Gas.Uint64(),
-			To:        address,                // preparedTx.To,
-			Value:     nil,                    // preparedTx.Value,
-		})}
+			ChainID:   big.NewInt(int64(270)),        // (*a.signer).Domain().ChainId,
+			Nonce:     0,                             // preparedTx.Nonce.Uint64(),
+			GasTipCap: big.NewInt(int64(0)),          // preparedTx.GasTipCap,
+			GasFeeCap: big.NewInt(int64(1000000000)), // preparedTx.GasFeeCap,
+			Gas:       21000,                         // preparedTx.Gas.Uint64(),
+			To:        address,                       // preparedTx.To,
+			Value:     big.NewInt(int64(100)),        // preparedTx.Value,
+			V:         big.NewInt(int64(872)),
+			R:         r,
+			S:         s,
+		})
 
-	
-	transactionWithSignature := transaction.WithSignature2()
+	// transactionWithSignature := transaction.WithSignature2()
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+
+	log.Println(transaction.Hash())
+	err = client.SendTransaction(context.Background(), transaction)
 	if err != nil {
 		log.Println(err)
 	}
-
-	err = client.SendTransaction(context.Background(), transactionWithSignature)
 }
